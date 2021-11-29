738f2725971af8408cd7901a0da1add0cbfe0b05
Leo Battaglia <ms@raptorizm.com>
2021-11-29 21:17:00 +0100

    Version 0.0.2

--- index.js
+++ index.js
@@ -3,94 +3,162 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.Encryptor = void 0;
 //Classes
 class Encryptor {
+    constructor() {
+        //Declarations
+        this.chars_letters_lower = "abcdefghijklmnopqrstuvwxyz";
+        this.chars_letters_upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        this.chars_numbers = "0123456789";
+        this.chars_password = ",.-;:_/*+{}[]()<>&?`'^!@#|$£€öÖäÄüÜ";
+        this.chars_password_exclude = "\\%=\"";
+    }
+    //Methods
+    countUsed(used) {
+        let count = 0;
+        for (let use of used) {
+            if (use) {
+                count++;
+            }
+        }
+        return count;
+    }
+    getChars(lower, upper, numbers, password) {
+        let chars = "";
+        lower ? chars += this.chars_letters_lower : undefined;
+        upper ? chars += this.chars_letters_upper : undefined;
+        numbers ? chars += this.chars_numbers : undefined;
+        password ? chars += this.chars_password : undefined;
+        return chars.split("");
+    }
+    getCharsRandomized(lower, upper, numbers, password) {
+        return this.randomizeArray(this.getChars(lower, upper, numbers, password));
+    }
+    /*getKey(length:number):string{
+        let chars:string[] = this.getCharsRandomized(true, true, true, false);
+        let key:string = "";
+        while(key.length < length){
+            let random = this.getRandomInt(0, chars.length - 1);
+            key += chars[random];
+        }
+        return key;
+    }*/
+    getRandomInt(min, max) {
+        if (max < min) {
+            let temp = min;
+            min = max;
+            max = temp;
+        }
+        max++;
+        let difference = max - min;
+        let ints = [];
+        for (let i = min; i < difference + min; i++) {
+            ints.push(i);
+        }
+        this.randomizeArray(ints);
+        return ints[this.getRandomIntPseudo(0, ints.length - 1)];
+    }
+    getRandomIntPseudo(min, max) {
+        if (max < min) {
+            let temp = min;
+            min = max;
+            max = temp;
+        }
+        max++;
+        let difference = max - min;
+        let random = Math.floor(Math.random() * difference);
+        random += min;
+        return random;
+    }
+    getUID(parts, length) {
+        let chars = this.getCharsRandomized(true, true, true, false);
+        let uuid = "";
+        for (let i = 0; i < parts; i++) {
+            let part = "";
+            while (part.length < length) {
+                let random = this.getRandomInt(0, chars.length - 1);
+                part += chars[random];
+            }
+            uuid += part;
+            if (i < 7) {
+                uuid += "-";
+            }
+        }
+        return uuid;
+    }
+    isChar(char) {
+        if (char.length === 1) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    randomizeArray(values) {
+        let used = new Array(values.length);
+        for (let i = 0; i < used.length; i++) {
+            used[i] = false;
+        }
+        while (this.countUsed(used) < values.length) {
+            let id1 = -1;
+            while (id1 < 0) {
+                if (this.countUsed(used) < (values.length * 0.8)) {
+                    let rnd = this.getRandomIntPseudo(0, values.length - 1);
+                    if (!used[rnd]) {
+                        used[rnd] = true;
+                        id1 = rnd;
+                    }
+                }
+                else {
+                    for (let i = 0; i < used.length; i++) {
+                        if (!used[i]) {
+                            used[i] = true;
+                            id1 = i;
+                            break;
+                        }
+                    }
+                }
+            }
+            let id2 = -1;
+            while (id2 < 0) {
+                let rnd = this.getRandomIntPseudo(0, values.length - 1);
+                if (rnd !== id1) {
+                    used[id2] = true;
+                    id2 = rnd;
+                }
+            }
+            this.getRandomIntPseudo(0, values.length - 1);
+            let temp = values[id1];
+            values[id1] = values[id2];
+            values[id2] = temp;
+        }
+        return values;
+    }
+    validateChar(char, lower, upper, numbers, password) {
+        if (this.isChar(char)) {
+            let chars = "";
+            lower ? chars += this.chars_letters_lower : undefined;
+            upper ? chars += this.chars_letters_upper : undefined;
+            numbers ? chars += this.chars_numbers : undefined;
+            password ? chars += this.chars_password : undefined;
+            if (chars.indexOf(char) > -1) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        else {
+            return false;
+        }
+    }
+    validateString(str, lower, upper, numbers, password) {
+        let split = str.split("");
+        for (let char of split) {
+            if (!this.validateChar(char, lower, upper, numbers, password)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
 exports.Encryptor = Encryptor;
-/*
-export const chars:string[] = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",
-                                "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D",
-                                "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S",
-                                "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7",
-                                "8", "9"];
-export const chars_password:string[] = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
-                                        "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
-                                        "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
-                                        "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
-                                        "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", "+", "-",
-                                        "*", "/", "<", ">", "ç", "%", "&", "(", ")", "?", "!", "[", "]",
-                                        ":", ",", "$", "£", "@", "#", "|", "¢", "ä", "Ä", "ö", "Ö", "ü",
-                                        "Ü", "_"];
- */
-/*
-
-export function getRandomInt(min:number, max:number):number{
-    if(max < min){
-        let temp:number = min;
-        min = max;
-        max = temp;
-    }
-    max++;
-    let difference:number = max - min;
-    let ints:number[] = [];
-    for(let i:number = min; i < difference + min; i++){
-        ints.push(i);
-    }
-    for(let i:number = 0; i < difference; i++){
-        let random:number = getRandomIntPseudo(0, ints.length - 1);
-        let random2:number = -1;
-        while(random2 === -1){
-            random2 = getRandomIntPseudo(0, ints.length - 1);
-            if(random2 === random){
-                random2 = -1;
-            }
-        }
-        let temp:number = ints[random];
-        ints[random] = ints[random2];
-        ints[random2] = temp;
-    }
-    return ints[getRandomIntPseudo(0, ints.length - 1)];
-}
-
-function getRandomIntPseudo(min:number, max:number):number{
-    if(max < min){
-        let temp:number = min;
-        min = max;
-        max = temp;
-    }
-    max++;
-    let difference:number = max - min;
-    let random:number = Math.floor(Math.random() * difference);
-    random += min;
-    return random;
-}
-
-
-export function getUUID():string{
-    let uuid:string = "";
-    for(let i:number = 0; i < chars.length; i++){
-        let random:number = getRandomInt(0, chars.length - 1);
-        let random2:number = -1;
-        while(random2 === -1){
-            random2 = getRandomInt(0, chars.length - 1);
-            if(random2 === random){
-                random2 = -1;
-            }
-        }
-        let temp:string = chars[random];
-        chars[random] = chars[random2];
-        chars[random2] = temp;
-    }
-    for(let i:number = 0; i < 8; i++){
-        let part = "";
-        while(part.length < 8){
-            let random = getRandomInt(0, chars.length - 1);
-            part += chars[random];
-        }
-        uuid += part;
-        if(i<7){
-            uuid += "-";
-        }
-    }
-    return uuid;
-}
- */ 
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,93 +1,164 @@
 //Classes
 export class Encryptor{
-    
-}
+    //Declarations
+    private readonly chars_letters_lower:string = "abcdefghijklmnopqrstuvwxyz";
+    private readonly chars_letters_upper:string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private readonly chars_numbers:string = "0123456789";
+    private readonly chars_password:string = ",.-;:_/*+{}[]()<>&?`'^!@#|$£€öÖäÄüÜ";
+    private readonly chars_password_exclude:string = "\\%=\"";
 
-/*
-export const chars:string[] = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",
-                                "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D",
-                                "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S",
-                                "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7",
-                                "8", "9"];
-export const chars_password:string[] = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
-                                        "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
-                                        "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
-                                        "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
-                                        "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", "+", "-",
-                                        "*", "/", "<", ">", "ç", "%", "&", "(", ")", "?", "!", "[", "]",
-                                        ":", ",", "$", "£", "@", "#", "|", "¢", "ä", "Ä", "ö", "Ö", "ü",
-                                        "Ü", "_"];
- */
-/*
-
-export function getRandomInt(min:number, max:number):number{
-    if(max < min){
-        let temp:number = min;
-        min = max;
-        max = temp;
-    }
-    max++;
-    let difference:number = max - min;
-    let ints:number[] = [];
-    for(let i:number = min; i < difference + min; i++){
-        ints.push(i);
-    }
-    for(let i:number = 0; i < difference; i++){
-        let random:number = getRandomIntPseudo(0, ints.length - 1);
-        let random2:number = -1;
-        while(random2 === -1){
-            random2 = getRandomIntPseudo(0, ints.length - 1);
-            if(random2 === random){
-                random2 = -1;
+    //Methods
+    private countUsed(used:Boolean[]):number{
+        let count:number = 0;
+        for(let use of used){
+            if(use){
+                count++;
             }
         }
-        let temp:number = ints[random];
-        ints[random] = ints[random2];
-        ints[random2] = temp;
+        return count;
     }
-    return ints[getRandomIntPseudo(0, ints.length - 1)];
-}
 
-function getRandomIntPseudo(min:number, max:number):number{
-    if(max < min){
-        let temp:number = min;
-        min = max;
-        max = temp;
+    getChars(lower:Boolean, upper:Boolean, numbers:Boolean, password:Boolean):string[]{
+        let chars:string = "";
+        lower ? chars += this.chars_letters_lower : undefined;
+        upper ? chars += this.chars_letters_upper : undefined;
+        numbers ? chars += this.chars_numbers : undefined;
+        password ? chars += this.chars_password : undefined;
+        return chars.split("");
     }
-    max++;
-    let difference:number = max - min;
-    let random:number = Math.floor(Math.random() * difference);
-    random += min;
-    return random;
-}
 
+    getCharsRandomized(lower:Boolean, upper:Boolean, numbers:Boolean, password:Boolean):string[]{
+        return this.randomizeArray(this.getChars(lower, upper, numbers, password));
+    }
 
-export function getUUID():string{
-    let uuid:string = "";
-    for(let i:number = 0; i < chars.length; i++){
-        let random:number = getRandomInt(0, chars.length - 1);
-        let random2:number = -1;
-        while(random2 === -1){
-            random2 = getRandomInt(0, chars.length - 1);
-            if(random2 === random){
-                random2 = -1;
+    /*getKey(length:number):string{
+        let chars:string[] = this.getCharsRandomized(true, true, true, false);
+        let key:string = "";
+        while(key.length < length){
+            let random = this.getRandomInt(0, chars.length - 1);
+            key += chars[random];
+        }
+        return key;
+    }*/
+
+    getRandomInt(min:number, max:number):number{
+        if(max < min){
+            let temp:number = min;
+            min = max;
+            max = temp;
+        }
+        max++;
+        let difference:number = max - min;
+        let ints:number[] = [];
+        for(let i:number = min; i < difference + min; i++){
+            ints.push(i);
+        }
+        this.randomizeArray(ints);
+        return ints[this.getRandomIntPseudo(0, ints.length - 1)];
+    }
+
+    getRandomIntPseudo(min:number, max:number):number{
+        if(max < min){
+            let temp:number = min;
+            min = max;
+            max = temp;
+        }
+        max++;
+        let difference:number = max - min;
+        let random:number = Math.floor(Math.random() * difference);
+        random += min;
+        return random;
+    }
+
+    getUID(parts:number, length:number){
+        let chars:string[] = this.getCharsRandomized(true, true, true, false);
+        let uuid:string = "";
+        for(let i:number = 0; i < parts; i++){
+            let part = "";
+            while(part.length < length){
+                let random = this.getRandomInt(0, chars.length - 1);
+                part += chars[random];
+            }
+            uuid += part;
+            if(i<7){
+                uuid += "-";
             }
         }
-        let temp:string = chars[random];
-        chars[random] = chars[random2];
-        chars[random2] = temp;
+        return uuid;
     }
-    for(let i:number = 0; i < 8; i++){
-        let part = "";
-        while(part.length < 8){
-            let random = getRandomInt(0, chars.length - 1);
-            part += chars[random];
-        }
-        uuid += part;
-        if(i<7){
-            uuid += "-";
+
+    isChar(char:string):Boolean{
+        if(char.length === 1){
+            return true;
+        }else{
+            return false;
         }
     }
-    return uuid;
+
+    randomizeArray(values:any[]):any[]{
+        let used:Boolean[] = new Array(values.length);
+        for(let i:number = 0; i < used.length; i++){
+            used[i] = false;
+        }
+        while(this.countUsed(used) < values.length){
+            let id1:number = -1;
+            while(id1 < 0){
+                if(this.countUsed(used) < (values.length * 0.8)){
+                    let rnd:number = this.getRandomIntPseudo(0, values.length - 1);
+                    if(!used[rnd]){
+                        used[rnd] = true;
+                        id1 = rnd;
+                    }
+                }else{
+                    for(let i:number = 0; i < used.length; i++){
+                        if(!used[i]){
+                            used[i] = true;
+                            id1 = i;
+                            break;
+                        }
+                    }
+                }
+            }
+            let id2:number = -1;
+            while(id2 < 0){
+                let rnd:number = this.getRandomIntPseudo(0, values.length - 1);
+                if(rnd !== id1){
+                    used[id2] = true;
+                    id2 = rnd;
+                }
+            }
+            this.getRandomIntPseudo(0, values.length - 1);
+            let temp = values[id1];
+            values[id1] = values[id2];
+            values[id2] = temp;
+        }
+        return values;
+    }
+
+    validateChar(char:string, lower:Boolean, upper:Boolean, numbers:Boolean, password:Boolean):Boolean{
+        if(this.isChar(char)){
+            let chars:string = "";
+            lower ? chars += this.chars_letters_lower : undefined;
+            upper ? chars += this.chars_letters_upper : undefined;
+            numbers ? chars += this.chars_numbers : undefined;
+            password ? chars += this.chars_password : undefined;
+            if(chars.indexOf(char) > -1){
+                return true;
+            }else{
+                return false;
+            }
+        }else{
+            return false;
+        }
+    }
+
+    validateString(str:string, lower:Boolean, upper:Boolean, numbers:Boolean, password:Boolean):Boolean{
+        let split:string[] = str.split("");
+        for(let char of split){
+            if(!this.validateChar(char, lower, upper, numbers, password)){
+                return false;
+            }
+        }
+        return true;
+    }
 }
- */
